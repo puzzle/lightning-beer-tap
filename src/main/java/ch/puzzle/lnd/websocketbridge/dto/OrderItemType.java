c82953465cb2ca963472b59a3bdb43a5a983c39c
Thomas Philipona <philipona@puzzle.ch>
2018-10-03 17:55:45 +0200

    Fixes OrderItemType

--- src/main/java/ch/puzzle/lnd/websocketbridge/dto/OrderItemType.java
+++ src/main/java/ch/puzzle/lnd/websocketbridge/dto/OrderItemType.java
@@ -1,10 +1,8 @@
 package ch.puzzle.lnd.websocketbridge.dto;
 
 public enum OrderItemType {
-    PRODUCT_1(5.0d),
-    PRODUCT_2(5.0d),
-    PRODUCT_3(5.0d),
-    PRODUCT_4(5.0d);
+	LARGE_BEER(2.0d),
+	SMALL_BEER(1.0d);
 
     private Double price;
 
