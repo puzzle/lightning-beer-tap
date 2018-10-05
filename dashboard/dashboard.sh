820379b45484d6ef2d5bf419fe1f06b2ed7e440b
aschmocker <schmocker@puzzle.ch>
2018-10-05 13:46:04 +0200

    Add kiosk args and change url to landscape

--- dashboard/dashboard.sh
+++ dashboard/dashboard.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SHOP="https://ln-self-order-pos-dev.ose3.puzzle.ch/#/self-service/puzzleUp"
+SHOP="https://ln-self-order-pos-dev.ose3.puzzle.ch/#/self-service-landscape/puzzleUp"
+KIOSK_ARGS="--kiosk --disable-translate --incognito --app=$SHOP"
 
 DISPLAY=:0 xset s off
 DISPLAY=:0 xset s noblank
 DISPLAY=:0 xset -dpms
 
-DISPLAY=:0 nohup chromium-browser --kiosk --disable-translate --incognito --app="$SHOP" &> /dev/null &
+DISPLAY=:0 nohup chromium-browser $KIOSK_ARGS &> /dev/null &
