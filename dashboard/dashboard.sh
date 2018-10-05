60ee124a3cb0ad48d1dd263152d4a838642e95c7
aschmocker <schmocker@puzzle.ch>
2018-10-05 13:48:18 +0200

    Add comments

--- dashboard/dashboard.sh
+++ dashboard/dashboard.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Arguments
 SHOP="https://ln-self-order-pos-dev.ose3.puzzle.ch/#/self-service-landscape/puzzleUp"
 KIOSK_ARGS="--kiosk --disable-translate --incognito --app=$SHOP"
 
+# Don't sleep, don't blank, waste energy!
 DISPLAY=:0 xset s off
 DISPLAY=:0 xset s noblank
 DISPLAY=:0 xset -dpms
 
+# Start app in kiosk mode
 DISPLAY=:0 nohup chromium-browser $KIOSK_ARGS &> /dev/null &
